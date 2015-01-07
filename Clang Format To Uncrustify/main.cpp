4bd0863e59642be5b27be92196f958c3bc9fac7f
Pranav Prakash <pranav-prakash@users.noreply.github.com>
2015-01-06 18:55:39 -0800

    Initial Commit

---
+++ Clang Format To Uncrustify/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Clang Format To Uncrustify
+//
+//  Created by Pranav Prakash on 1/6/15.
+//  Copyright (c) 2015 Pranav Prakash. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
